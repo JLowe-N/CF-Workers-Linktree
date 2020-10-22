9bcb00195b70b8702e9a47de56d04b3f7db7cc41
Justin Lowen <Justin.G.Lowen@gmail.com>
2020-10-22 14:55:46 -0500

    Implement HTMLRewriter for inserting links

--- index.js
+++ index.js
@@ -1,15 +1,57 @@
+const workerDomain = 'https://worker.justinlowen.workers.dev'
+
+
 addEventListener('fetch', event => {
-  event.respondWith(handleRequest(event.request))
+  return event.respondWith(handleRequest(event.request))
 })
-/**
- * Respond with hello worker text
- * @param {Request} request
- */
+
 
 async function handleRequest(request) {
+  let response
+
+  if (request.method === 'GET' & request.url === `${workerDomain}/links`) {
+    response = new Response(JSON.stringify(testLinks), {
+      headers: { 'content-type': 'JSON' },
+    })
+  } else {
+    let request = await fetch('https://static-links-page.signalnerve.workers.dev')
+    response = new HTMLRewriter().on("div#links", new ElementHandler()).transform(request)
+    response = await response.text()
+    response = new Response(response, {
+      headers: { "content-type": "text/html;charset=UTF-8", "status": 200 }
+    })
+    console.log(response)
 
 
-  return new Response('Hello worker!', {
-    headers: { 'content-type': 'JSON' },
-  })
+  }
+
+  return response
 }
+
+
+class ElementHandler {
+  element(element) {
+    let linkHtml = ''
+    testLinks.forEach(link => {
+      linkHtml += `<a href="${link.url}">${link.name}</a>`
+    })
+    element.setInnerContent(linkHtml, { html: true })
+  }
+}
+
+
+
+let testLinks = [
+  {
+    "name": "My GitHub",
+    "url": "https://github.com/JLowe-N"
+  },
+  {
+    "name": "Portfolio",
+    "url": "https://JLowe-N.github.io"
+  },
+  {
+    "name": "LinkedIn",
+    "url": "https://www.linkedin.com/in/justinlowen"
+  }
+]
